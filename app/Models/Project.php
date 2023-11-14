66abaa7ce38fa883aab19aed926d3cb824d3d766
EsterDahri <esterdahri@gmail.com>
2023-11-14 14:26:35 +0100

    Initial commit

---
+++ app/Models/Project.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'slug', 'cover_image', 'content', 'type_id', 'github', 'link'];
+
+    public function generateSlug($title)
+    {
+        return Str::slug($title, '-');
+    }
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
+}
